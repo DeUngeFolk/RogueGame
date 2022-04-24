51c81aa1ba676132e7474f366dc6803c3e96b30b
AndersLind99 <cph-al350@cphbusiness.dk>
2022-04-25 01:05:48 +0200

    fixed vscode issue
    fixed issue where vscode wasn't properly connected to the unity project.

---
+++ Assets/Script/test.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour, IAttackable
+{
+    public int currentHealth => throw new System.NotImplementedException();
+
+    public void takeDamage(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+}
