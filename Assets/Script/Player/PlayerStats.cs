194d4bbc05c82514df6a00b3d1c272bdbf1ea8c0
AndersLind99 <cph-al350@cphbusiness.dk>
2022-05-11 14:05:27 +0200

    dash
    implemented basic dash

--- Assets/Script/Player/PlayerStats.cs
+++ Assets/Script/Player/PlayerStats.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class PlayerStats : MonoBehaviour, IAttackable
 {
-
-    public int maxHealth{get; private set;}
+    public int maxHealth { get; private set; }
 
     public bool alive { get; private set; }
 
     public int currentHealth { get; private set; }
 
-    public HealthBar healthBar;
+    float curTime = 0;
 
+    float nextDamage = 1;
+
+    public HealthBar healthBar;
 
     // Start is called before the first frame update
     void Start()
     {
         maxHealth = 100;
         currentHealth = maxHealth;
-        healthBar.SetMaxHealth(maxHealth);
+        healthBar.SetMaxHealth (maxHealth);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-
         if (0 >= currentHealth)
         {
             death();
@@ -36,12 +36,16 @@ public class PlayerStats : MonoBehaviour, IAttackable
     void OnTriggerEnter2D(Collider2D col)
     {
         GameObject enemy = col.gameObject;
-
-        if(enemy != null){
+        if (enemy != null)
+        {
             takeDamage(5);
             Debug.Log("player has been hit!");
-
         }
+    }
+
+    void onTriggerExit2D()
+    {
+        Debug.Log("exited collider");
     }
 
     void death()
@@ -49,12 +53,9 @@ public class PlayerStats : MonoBehaviour, IAttackable
         Destroy (gameObject);
     }
 
-    
-
     public void takeDamage(int damage)
     {
         currentHealth = currentHealth - damage;
-        healthBar.SetHealth(currentHealth);
-        
+        healthBar.SetHealth (currentHealth);
     }
 }
