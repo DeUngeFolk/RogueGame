8beefc4451ab3f6ae854d5b1b4b19e9168ef5531
AndersLind99 <cph-al350@cphbusiness.dk>
2022-06-23 02:17:33 +0200

    played with tilemap & made aim a lil better

--- Assets/Script/Player/Mouse_Aim.cs
+++ Assets/Script/Player/Mouse_Aim.cs
@@ -4,29 +4,26 @@ using UnityEngine;
 
 public class Mouse_Aim : MonoBehaviour
 {
-    public Rigidbody2D firePoint;
+    public Rigidbody2D rb;
+
     public Camera cam;
 
-    Vector2 mousePosition;
+    public Transform playerTransform;
 
+    Vector2 mousePosition;
 
     // Update is called once per frame
     void Update()
     {
-
-
         mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
-
+        rb.position =
+            new Vector2(playerTransform.position.x, playerTransform.position.y);
     }
 
     void FixedUpdate()
     {
-
-        Vector2 lookDir = mousePosition - firePoint.position;
+        Vector2 lookDir = mousePosition - rb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        firePoint.rotation = angle;
-
-
-
+        rb.rotation = angle;
     }
 }
